df8e74d9b6f04ba723e688c5295760050a315e2f
fperez08 <francisco.pgarcia28@gmail.com>
2025-01-23 17:46:04 -0600

    🐛 (pageObjects/Login.ts, pageObjects/Signup.ts): Update Playwright test type imports and selectors for better consistency and maintainability

--- pageObjects/Login.ts
+++ pageObjects/Login.ts
@@ -1,17 +1,17 @@
-import type { Page } from "@playwright/test";
+import type { Page } from '@playwright/test';
 
 export class Login {
   constructor(private readonly page: Page) {}
   clicks_on = {
     remember_me: async () => {
-      await this.page.getByLabel("Remember me").click();
+      await this.page.getByLabel('Remember me').click();
     },
     sign_in: async () => {
-      await this.page.getByRole("button", { name: "SIGN IN" }).click();
+      await this.page.getByRole('button', { name: 'SIGN IN' }).click();
     },
     sign_up: async () => {
       await this.page
-        .getByRole("link", {
+        .getByRole('link', {
           name: /don't have an account\? sign up/i,
         })
         .click();
@@ -20,7 +20,7 @@ export class Login {
   types_into = {
     user_name: async (userName: string) => {
       await this.page
-        .getByRole("textbox", { name: /username/i })
+        .getByRole('textbox', { name: /username/i })
         .fill(userName);
     },
     password: async (password: string) => {
@@ -29,7 +29,7 @@ export class Login {
   };
   navigates_to = {
     sign_in: async () => {
-      await this.page.goto("/signin");
+      await this.page.goto('/signin');
     },
   };
 }
